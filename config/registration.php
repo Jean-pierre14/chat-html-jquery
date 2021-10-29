ff10f6a4a4d0ad1ba08e8ba801aaf2d2469fe4ed
User <chiruzabisimwa@gmail.com>
2021-10-29 06:00:46 +0200

    Cors issue, the problem to fix

--- config/registration.php
+++ config/registration.php
@@ -1,4 +1,4 @@
 <?php
-    include_once "./config.php";
-    print "Data";
+    // include_once "./config.php";
+    echo "Data";
 ?>
--- js/registration.js
+++ js/registration.js
@@ -2,17 +2,20 @@ const form = document.querySelector('form.form-data'),
     submitBtn = form.querySelector(".btn-php");
 
 form.onsubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 }
 
-submitBtn.addEventListener('click', function () {
-    alert("Cool")
-})
+
+// submitBtn.addEventListener('click', function () { // To check if the eventListener it works correctely
+//     alert("Cool")
+// })
 
 submitBtn.onclick = () => {
+    alert("Cool to")
     // Ajax
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "config/registration.php", true);
+    // xhr.open("POST", "config/registration.php", true);
+    xhr.open("POST", "test.php", true);
     xhr.onload = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
@@ -21,4 +24,5 @@ submitBtn.onclick = () => {
             }
         }
     }
+    xhr.send();
 }
