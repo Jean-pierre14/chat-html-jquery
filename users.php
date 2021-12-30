b498e5af36b5b45f136025929517ad54d1d731f3
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-12-30 07:13:05 +0100

    The extension and header configuration

--- users.php
+++ users.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat App</title>
-    <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/58594917?v=4" type="image/x-icon">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/lightbox.min.css">
-    <link rel="stylesheet" href="./css/progress.css">
-</head>
+<?php include_once "header.php";?>
 
 <body>
     <div class="container">
